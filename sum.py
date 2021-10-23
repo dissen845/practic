d5f022b1cf3db7b9e0e03d89387de6c2c420a57d
dissen845 <bleckmesa@mail.ru>
2021-10-23 12:39:30 +0300

    добавлена функция минуса

--- sum.py
+++ sum.py
@@ -2,5 +2,10 @@ def pluse(a, b):
     """Складывает два числа"""
     return a + b
 
+def minus(a, b):
+    """Вычитает из а число b"""
+    return a - b
+
 if __name__ == '__main__':
     print(pluse(2, 2))
+    print(minus(4, 2))
