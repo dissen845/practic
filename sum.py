336a20a010898b374bf109dc7ee34b52c8c63e53
dissen845 <bleckmesa@mail.ru>
2021-10-23 12:45:23 +0300

    добавлена функция умножения и добавлена новая функция в новом файле upper

--- sum.py
+++ sum.py
@@ -6,6 +6,11 @@ def minus(a, b):
     """Вычитает из а число b"""
     return a - b
 
+def mul(a, b):
+    """Умножает два числа"""
+    return  a*b
+
 if __name__ == '__main__':
     print(pluse(2, 2))
     print(minus(4, 2))
+    print((mul(3, 3)))
